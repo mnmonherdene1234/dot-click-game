2db0d454c6552646c482e8bf870ebc560e839b3e
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2023-07-01 15:38:13 +0800

    icon, meta data, css added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 import { Dot } from "./dot.js";
 import clickedCount from "./clicked-count.js";
-import data from "./data.js";
 
 const dots = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 20; i++) {
   const dot = Dot.randomPositionDot();
   dots.push(dot);
   document.body.append(dot.element);
